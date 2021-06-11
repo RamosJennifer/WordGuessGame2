15c4fa546f549871ee237a469f0a9633da4c6d6a
Jennifer Ramos <jenniferramos4858@gmail.com>
2021-06-11 14:32:04 -0500

    modifying the current word to display as a string after converting from an array

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,34 @@
 var charachters = ["Rory", "Lorelai", "Luke", "Sookie", "Michel", "Emily", "Richard", "Lane", "Paris", "Madeline", "Louise", "Dean", "Jess", "Logan"]
 var randomize = charachters[Math.floor(Math.random() * charachters.length)];
 var currentWord = randomize.toLowerCase();
-var underscore = "__ ";
+var underscore = "_";
 var underscoredWord = underscore.repeat(currentWord.length)
 var lettersGuessed = [];
+cwArray = Array.from(currentWord);
+usArray = Array.from(underscoredWord);
 
 console.log(currentWord);
-console.log(currentWord.length);
-document.getElementById("currentWord").innerHTML = underscoredWord;
-// document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
+
+for (i = 0; i < usArray.length; i++) {
+    // console.log(i);
+    document.getElementById("currentWord").innerHTML = (usArray[i] + " ").repeat(usArray.length);
+}
 
 
-document.onkeyup = function(event){
+document.onkeyup = function (event) {
     var userGuess = event.key;
     lettersGuessed.push(userGuess);
-    
-    
-    for (i=0;i<currentWord.length;i++){
-        if (userGuess === currentWord[i]){
-            alert("hit at index " + [i]);
-            // underscoredWord[currentWord[i]].push(userGuess);
+
+    for (i = 0; i < cwArray.length; i++) {
+        var index = 0;
+
+        if (userGuess === cwArray[i]) {
+            index = i;
+            usArray.splice(index, 1, userGuess);
+            document.getElementById("currentWord").innerHTML = (usArray).join(' ')
         }
-        if (userGuess != currentWord[i]){
+        if (userGuess != cwArray[i]) {
+
             document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
         }
     }
