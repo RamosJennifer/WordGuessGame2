2903e41efe7d9586461669e010d801473bb5cf9f
Jennifer Ramos <jenniferramos4858@gmail.com>
2021-06-14 14:14:46 -0500

    currentWord updates with each newRound

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,13 +1,13 @@
-var charachters = ["Rory", "Lorelai", "Luke", "Sookie", "Michel", "Emily", "Richard", "Lane", "Paris", "Madeline", "Louise", "Dean", "Jess", "Logan"]
+var charachters = ["Rory", "Lorelai", "Luke", "Sookie", "Michel", "Emily", "Richard", "Lane", "Paris", "Madeline", "Louise", "Dean", "Jess", "Logan", "Christopher", "Jackson", "Babette", "Paty", "Taylor", "Kirk"]
 var randomize = charachters[Math.floor(Math.random() * charachters.length)]
 var currentWord = randomize.toLowerCase();
 var underscore = "_";
 var underscoredWord = underscore.repeat(currentWord.length)
 var lettersGuessed = [];
 var wins = 0;
+var guessesRemaining = 10;
 cwArray = Array.from(currentWord);
 usArray = Array.from(underscoredWord);
-// console.log(currentWord);
 
 for (i = 0; i < usArray.length; i++) {
     document.getElementById("currentWord").innerHTML = (usArray[i] + " ").repeat(usArray.length);
@@ -15,27 +15,25 @@ for (i = 0; i < usArray.length; i++) {
 
 function newRound() {
     lettersGuessed = [];
-    var randomize = charachters[Math.floor(Math.random() * charachters.length)];
-    var currentWord = randomize.toLowerCase();
-    var underscore = "_";
-    var underscoredWord = underscore.repeat(currentWord.length);
+    guessesRemaining = 10;
+    randomize = charachters[Math.floor(Math.random() * charachters.length)];
+    currentWord = randomize.toLowerCase();
+    underscore = "_";
+    underscoredWord = underscore.repeat(currentWord.length);
     cwArray = Array.from(currentWord);
     usArray = Array.from(underscoredWord);
-    // console.log(currentWord);
+    
 
     for (i = 0; i < usArray.length; i++) {
         document.getElementById("currentWord").innerHTML = (usArray[i] + " ").repeat(usArray.length);
     }
 }
 
-
 document.onkeyup = function (event) {
-    var userGuess = event.key;
-    lettersGuessed.push(userGuess);
+    var userGuess = event.key.toLowerCase();
 
     for (i = 0; i < cwArray.length; i++) {
         var index = 0;
-
         if (userGuess === cwArray[i]) {
             index = i;
             usArray.splice(index, 1, userGuess);
@@ -43,17 +41,32 @@ document.onkeyup = function (event) {
             document.getElementById("currentWord").innerHTML = usturnedString;
             var cwturnedString = cwArray.join(' ');
             if (usturnedString === cwturnedString) {
-                alert("You win!");
-                // Wins
+                guessesRemaining = 0;
                 wins++
                 document.getElementById("wins").innerHTML = wins;
-                newRound();
+                showAnswer();
             }
         }
-        if (userGuess != cwArray[i]) {
+    }
+    if (userGuess !== cwArray[i]) {
+        guessesRemaining--;
+        document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
 
+        // -----------------------------------------------------------------------
+
+        // -----------------------------------------------------------------------
+
+        if (usturnedString === cwturnedString) {
+            lettersGuessed.push(userGuess);
             document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
         }
+        if (guessesRemaining === 0) {
+            showAnswer();
+        }
+    }
+    function showAnswer() {
+        alert("The cwTurnedString " + currentWord);
+        newRound();
     }
 }
 
