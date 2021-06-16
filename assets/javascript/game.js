8db50beb32dd45fe3cf5aab7aa012d27a9311526
Jennifer Ramos <jenniferramos4858@gmail.com>
2021-06-16 10:50:30 -0500

    adding a bootsrap hyperlink and incorporating bootstrap framework to game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,21 +1,29 @@
 var charachters = ["Rory", "Lorelai", "Luke", "Sookie", "Michel", "Emily", "Richard", "Lane", "Paris", "Madeline", "Louise", "Dean", "Jess", "Logan", "Christopher", "Jackson", "Babette", "Paty", "Taylor", "Kirk"]
+// Randomizing Charachters
 var randomize = charachters[Math.floor(Math.random() * charachters.length)]
 var currentWord = randomize.toLowerCase();
+// Converting randomized word into an underscored string
 var underscore = "_";
 var underscoredWord = underscore.repeat(currentWord.length)
+// Empty & Begining Count Variables
 var lettersGuessed = [];
 var wins = 0;
 var guessesRemaining = 10;
+// Array>String>Array
 cwArray = Array.from(currentWord);
 usArray = Array.from(underscoredWord);
 
+// Starting game underscored word
 for (i = 0; i < usArray.length; i++) {
     document.getElementById("currentWord").innerHTML = (usArray[i] + " ").repeat(usArray.length);
 }
 
+// user controlled function
 document.onkeyup = function (event) {
+    // tracking users actions
     var userGuess = event.key.toLowerCase();
-
+    
+    // New Round
     function newRound() {
         lettersGuessed = [];
         guessesRemaining = 10;
@@ -30,6 +38,7 @@ document.onkeyup = function (event) {
         }
     }
 
+    // If user guess is correct
     for (i = 0; i < cwArray.length; i++) {
         var index = 0;
         if (userGuess === cwArray[i]) {
@@ -42,31 +51,35 @@ document.onkeyup = function (event) {
                 guessesRemaining = 1;
                 wins++
                 document.getElementById("wins").innerHTML = wins;
-                showAnswer();
-                // newRound();
             }
         }
     }
+    // if user guess is incorrect
     if (userGuess !== cwArray[i]) {
         guessesRemaining--;
         document.getElementById("guessesRemaining").innerHTML = guessesRemaining;
-
-        // -----------------------------------------------------------------------
-
-        // -----------------------------------------------------------------------
 
         if (usturnedString === cwturnedString) {
             lettersGuessed.push(userGuess);
             document.getElementById("lettersGuessed").innerHTML = lettersGuessed;
         }
-        if (guessesRemaining <= 0) {
+        if (guessesRemaining === 0) {
             showAnswer();
             newRound();
         }
     }
+    // --------------------------------------------------------
+     // if (userGuess === ){
+    //     console.log("This letter has been guessed before.")
+    // }
+    // console.log("-------------------------------")
+    //----------------------------------------------------------
+   
     function showAnswer() {
-        alert("The cwTurnedString " + currentWord);
+        alert("The answer was " + currentWord);
     }
 }
+
+
 
 
